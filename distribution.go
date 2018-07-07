ee407dc4104ca5a653a258bf5afceb58efbd4baf
Shoichi Kaji <skaji@cpan.org>
2018-07-07 18:19:04 +0900

    fetch mata, and generate "name" link

--- distribution.go
+++ distribution.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"regexp"
 	"strings"
 )
@@ -28,10 +32,10 @@ func NewDistribution(body string) (*Distribution, error) {
 
 	r := res[0]
 	d := Distribution{
-		CPANID:    r[1],
-		Distvname: r[3],
+		CPANID:    r[1], // SKAJI
+		Distvname: r[3], // App-cpm-0.987
 		IsPerl6:   false,
-		Pathname:  r[0],
+		Pathname:  r[0], // S/SK/SKAJI/App-cpm-0.987.tar.gz
 	}
 	if r[2] == "Perl6/" {
 		d.IsPerl6 = true
@@ -50,6 +54,14 @@ func NewDistribution(body string) (*Distribution, error) {
 
 	d.Distname = strings.Join(parts[:len(parts)-1], "-")
 	d.MainModule = strings.Join(parts[:len(parts)-1], "::")
+	if d.IsPerl6 {
+		name, err := d.guessPerl6MainModule()
+		if err == nil {
+			d.MainModule = name
+		} else {
+			log.Println(err)
+		}
+	}
 	return &d, nil
 }
 
@@ -61,6 +73,51 @@ type Distribution struct {
 	MainModule string `json:"main_module"`
 	IsPerl6    bool   `json:"perl6"`
 	Pathname   string `json:"pathname"`
+}
+
+func (d *Distribution) fetchPerl6Meta() ([]byte, error) {
+	if len(d.CPANID) < 2 {
+		return nil, errors.New("too short CPANID")
+	}
+	url := fmt.Sprintf(
+		"https://cpan.metacpan.org/authors/id/%s/%s/%s/Perl6/%s.meta",
+		string(d.CPANID[0:1]),
+		string(d.CPANID[0:2]),
+		d.CPANID,
+		d.Distvname,
+	)
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s, %s", res.Status, url)
+	}
+	return body, nil
+}
+
+type distributionMeta struct {
+	Name string `json:"name"`
+}
+
+func (d *Distribution) guessPerl6MainModule() (string, error) {
+	body, err := d.fetchPerl6Meta()
+	if err != nil {
+		return "", err
+	}
+	var meta distributionMeta
+	if err := json.Unmarshal(body, &meta); err != nil {
+		return "", err
+	}
+	if name := meta.Name; name != "" {
+		return name, nil
+	}
+	return "", errors.New("cannot find suitable main module name from 'name' in meta")
 }
 
 // Summary is
