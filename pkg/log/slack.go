df5634938a424873cbdf20be9772442f6df852c1
Shoichi Kaji <skaji@cpan.org>
2019-06-20 07:52:12 +0900

    chan

--- pkg/log/slack.go
+++ pkg/log/slack.go
@@ -17,12 +17,19 @@ import (
 // SlackLogger is
 type SlackLogger struct {
 	url string
+	ch  chan string
 	*base.Logger
 }
 
 // NewSlack is
 func NewSlack(url string) Logger {
-	return &SlackLogger{url, base.New(os.Stderr, "", base.LstdFlags|base.Llongfile)}
+	l := &SlackLogger{
+		url:    url,
+		ch:     make(chan string, 1000),
+		Logger: base.New(os.Stderr, "", base.LstdFlags|base.Llongfile),
+	}
+	go l.poster()
+	return l
 }
 
 // Fatal is
@@ -32,20 +39,36 @@ func (l *SlackLogger) Fatal(v ...interface{}) {
 
 // Printf is
 func (l *SlackLogger) Printf(format string, v ...interface{}) {
-	go l.Post(fmt.Sprintf(format, v...))
+	l.Post(fmt.Sprintf(format, v...))
 	l.Logger.Printf(format, v...)
 }
 
 // Println is
 func (l *SlackLogger) Println(v ...interface{}) {
-	go l.Post(fmt.Sprintln(v...))
+	l.Post(fmt.Sprintln(v...))
 	l.Logger.Println(v...)
 }
 
 // Post is
 func (l *SlackLogger) Post(text string) {
-	if err := l.post(text); err != nil {
-		l.Logger.Println(err)
+	select {
+	case l.ch <- text:
+	default:
+		l.Logger.Println("slack channel is full, skip", text)
+	}
+}
+
+func (l *SlackLogger) poster() {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		text := <-l.ch
+		go func() {
+			if err := l.post(text); err != nil {
+				l.Logger.Println(err)
+			}
+		}()
 	}
 }
 
